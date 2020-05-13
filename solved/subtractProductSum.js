93a21723d936fbcfbf1a03722d19e0fe753a422b
Justin <me@justinle.page>
2020-05-13 01:38:52 -0700

    solved

---
+++ solved/subtractProductSum.js
@@ -0,0 +1,18 @@
+/*
+LeetCode #1281
+
+Given an integer number n, return the difference between the
+product of its digits and the sum of its digits.
+*/
+
+const subtractProductAndSum = (n) => {
+    const split = [...(n + ``)].map(Number);
+    const product = split.reduce((a, b) => a * b);
+    const sum = split.reduce((a, b) => a + b);
+
+    return product - sum;
+};
+// 52ms and 34.1mb
+
+subtractProductAndSum(234); // 15
+subtractProductAndSum(4421); // 21
