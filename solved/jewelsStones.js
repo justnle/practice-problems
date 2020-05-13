3b0a414b7a9e15f94bd8405576691317670293ae
Justin <me@justinle.page>
2020-05-12 21:40:49 -0700

    solved

--- solved/jewelsStones.js
+++ solved/jewelsStones.js
@@ -10,23 +10,35 @@ are letters. Letters are case sensitive, so "a" is considered a different
 type of stone from "A".
 */
 
-const numJewelsInStones = (J, S) => {
-    let count = 0;
-    const jewels = J.split(``);
-    const stones = S.split(``);
+// const numJewelsInStones = (J, S) => {
+//     let count = 0;
+//     const jewels = J.split(``);
+//     const stones = S.split(``);
 
-    if (J.length === 0 || S.length === 0) {
-        return 0;
-    }
+//     if (J.length === 0 || S.length === 0) {
+//         return 0;
+//     }
 
-    for (const stone of stones) {
-        for (let i = 0; i < jewels.length; ++i) {
-            jewels[i] === stone ? ++count : count;
-        }
-    }
-    return count;
-};
+//     for (const stone of stones) {
+//         for (let i = 0; i < jewels.length; ++i) {
+//             jewels[i] === stone ? ++count : count;
+//         }
+//     }
+//     return count;
+// };
 // 64ms and 35.4mb .. O(n^2)
+
+const numJewelsInStones = (J, S) => {
+    const jewelMap = J.split(``).reduce((obj, j) => {
+        obj[j] = true;
+        console.log(obj);
+        return obj;
+    }, {});
+    return S.split(``).reduce(
+        (count, s) => (jewelMap[s] ? count + 1 : count),
+        0
+    );
+};
 
 numJewelsInStones(`aA`, `aAAbbbb`); // expected 3
 numJewelsInStones(`z`, `ZZ`); // 0
