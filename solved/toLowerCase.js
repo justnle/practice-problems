55e05cf4e4fcafa9621fac413c872129b1937efa
Justin <me@justinle.page>
2020-05-14 18:13:50 -0700

    too easy?

---
+++ solved/toLowerCase.js
@@ -0,0 +1,15 @@
+/*
+LeetCode #709
+
+Implement function ToLowerCase() that has a string parameter str,
+and returns the same string in lowercase.
+ */
+
+const toLowerCase = (str) => {
+    console.log(str.toLowerCase());
+};
+// 40ms and 33.7mb, 99.20% faster
+
+toLowerCase(`Hello`); // hello
+toLowerCase(`here`); // here
+toLowerCase(`LOVELY`); // lovely
