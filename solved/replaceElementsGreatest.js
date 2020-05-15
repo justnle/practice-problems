e3ca5b5d1c13b5ee5cac6f19c05b874786a18edb
Justin <me@justinle.page>
2020-05-14 22:52:17 -0700

    solved, VERY slow

---
+++ solved/replaceElementsGreatest.js
@@ -0,0 +1,33 @@
+/*
+LeetCode #1299
+
+Given an array arr, replace every element in that array with the greatest
+element among the elements to its right, and replace the last element
+with -1.
+
+After doing so, return the array.
+*/
+const replaceElements = (arr) => {
+    const answer = [];
+    const len = arr.length - 1;
+
+    for (let i = 0; i < len; ++i) {
+        arr.shift();
+        answer.push(Math.max(...arr));
+    }
+    answer.push(-1);
+    return answer;
+};
+// 460ms and 38.8mb, 23.81% faster
+
+/*
+pseudocode
+start at element 0 and find the biggest value after
+replace element at 0 with biggest value
+element 1, find the biggest value after
+replace it
+... etc
+replace last value with -1
+*/
+
+replaceElements([17, 18, 5, 4, 6, 1]); // [18,6,6,6,1,-1]
