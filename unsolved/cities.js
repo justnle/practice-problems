d6816cdbccb8c96ab4f06af8d929b66babd0ae53
Justin <me@justinle.page>
2021-07-22 13:12:28 -0700

    unsolved problems

---
+++ unsolved/cities.js
@@ -0,0 +1,51 @@
+/*
+There are n cities. Some of them are connected, while
+some are not. If city a is connected directly to city b,
+and city b is connected directly with city c, then city a
+is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected
+cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1
+if the i^th city and the j^th city are directly connected, and
+isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+*/
+
+/*
+PSEUDOCODE
+
+1. find the length of isConnected to determine max amount of provinces
+2. map over the first array
+    if index 1 or index 2 === 1, then city a is connected to b and/or c
+3. map over the second array
+    if index 0 or index 2 === 1, then city b is connected to a and/or c
+*/
+
+const findCircleNum = (isConnected) => {
+    let maxProv = isConnected.length;
+
+    for (let i = 0; i < isConnected.length; ++i) {
+        if (
+            JSON.stringify(isConnected[i]) ===
+            JSON.stringify(isConnected[i + 1])
+        ) {
+            --maxProv;
+        }
+    }
+
+    console.log(maxProv);
+};
+
+findCircleNum([
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1]
+]); // Output 2
+findCircleNum([
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+]); // Output 3
---
+++ unsolved/sortedArray.js
@@ -0,0 +1,51 @@
+/*
+Given a sorted array of distinct integers and a target value,
+return the index if the target is found. If not, return the
+index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+/*
+PSEUDOCODE
+
+- map over array
+- look for target
+    - if target is found, return index of target
+    - if target is not found, find index where array 
+      element is less than or equal to target
+       return index +1 of that index
+-  edge cases
+    if target = 0, return 0
+*/
+
+const searchInsert = (nums, target) => {
+    let found = false;
+    let previous;
+
+    if (target === 0) {
+        return 0;
+    }
+
+    for (const num of nums) {
+        if (num === target) {
+            found = true;
+            return nums.indexOf(num);
+        }
+    }
+
+    if (found === false) {
+        for (const num of nums) {
+            if (num <= target) {
+                previous = num;
+            }
+        }
+        return nums.indexOf(previous) + 1;
+    }
+};
+
+searchInsert([1, 3, 5, 6], 5);
+searchInsert([1, 3, 5, 6], 2);
+searchInsert([1, 3, 5, 6], 7);
+searchInsert([1, 3, 5, 6], 0);
+searchInsert([1], 0);
