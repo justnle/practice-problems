6947fd9e9c513f6319a74d7deb4a2e6bb381479f
Justin <info@justinle.me>
2021-09-29 13:03:27 -0700

    solved

---
+++ solved/LeetCode/0088_mergeSortedArray.js
@@ -0,0 +1,12 @@
+const merge = (nums1, m, nums2, n) => {
+    let idx1 = m - 1;
+    let idx2 = n - 1;
+    let idx3 = m + n - 1;
+
+    while (idx2 >= 0) {
+        nums1[idx3--] =
+            nums1[idx1] > nums2[idx2] ? nums1[idx1--] : nums2[idx2--];
+    }
+};
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3); // [1,2,2,3,5,6];
