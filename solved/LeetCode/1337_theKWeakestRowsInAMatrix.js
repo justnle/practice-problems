94efa1e666cd5cd1691e1b37870b328279a03e14
Justin <info@justinle.me>
2022-03-26 18:32:52 -0700

    solved, first try :)

---
+++ solved/LeetCode/1337_theKWeakestRowsInAMatrix.js
@@ -0,0 +1,89 @@
+/*
+LeetCode 1337 - The K Weakest Rows in a Matrix - Easy
+
+You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's
+(representing civilians). The soldiers are positioned in front of the civilians.
+That is, all the 1's will appear to the left of all the 0's in each row.
+
+A row i is weaker than a row j if one of the following is true:
+    - The number of soldiers in row i is less than the number of soldiers
+    in row j.
+    - Both rows have the same number of soldiers and i < j.
+
+Return the indices of the k weakest rows in the matrix ordered from the weakest
+to strongest.
+
+Example 1:
+Input: mat =
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]]
+k = 3
+Output: [2,0,3]
+
+Explanation:
+The number of soldiers in each row is:
+- Row 0: 2
+- Row 1: 4
+- Row 2: 1
+- Row 3: 2
+- Row 4: 5
+The rows ordered from weakest to strongest are [2,0,3,1,4].
+
+Example 2:
+Input: mat =
+[[1,0,0,0],
+ [1,1,1,1],
+ [1,0,0,0],
+ [1,0,0,0]]
+k = 2
+Output: [0,2]
+
+Explanation:
+The number of soldiers in each row is:
+- Row 0: 1
+- Row 1: 4
+- Row 2: 1
+- Row 3: 1
+The rows ordered from weakest to strongest are [0,2,3,1].
+*/
+
+const kWeakestRows = (mat, k) => {
+    const numOfSoldiers = new Map();
+    const res = [];
+
+    for (const row of mat) {
+        let soldiers = 0;
+
+        for (const person of row) {
+            if (person === 1) {
+                ++soldiers;
+            }
+        }
+        numOfSoldiers.set(mat.indexOf(row), soldiers);
+    }
+
+    const soldierArr = [...numOfSoldiers].sort((a, b) => a[1] - b[1]);
+
+    for (let i = 0; i < k; ++i) {
+        res.push(soldierArr[i][0]);
+    }
+
+    return res;
+};
+
+kWeakestRows(
+    [
+        [1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 1]
+    ],
+    3
+); // [2,0,3]
+
+// 88ms, faster than 6.24% of js submissions
+// 44.4mb, less than 48.73% of js submissions
