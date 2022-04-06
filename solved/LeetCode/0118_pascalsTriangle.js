5facf0493179f9530f28974b60c5270c799241aa
Justin <info@justinle.me>
2022-04-05 21:58:59 -0700

    solved

---
+++ solved/LeetCode/0118_pascalsTriangle.js
@@ -0,0 +1,36 @@
+/*
+LeetCode #118 - Pascal's Triangle - Easy
+
+Given an integer numRows, generate the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example 1:
+Input: numRows = 5;
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+Input: numRows = 1;
+Output: [[1]]
+*/
+
+const generate = (numRows) => {
+    const res = [];
+
+    for (let i = 0; i < numRows; ++i) {
+        res[i] = [];
+        res[i][0] = 1;
+
+        for (let j = 1; j < i; ++j) {
+            res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+        }
+        res[i][i] = 1;
+    }
+    return res;
+};
+
+generate(5); // [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+generate(1); // [[1]]
+
+// 72ms, faster than 62.97% of js submissions
+// 42mb, less than 55.64% of js submissions
