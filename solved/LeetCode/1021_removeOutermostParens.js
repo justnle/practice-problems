40f677dc0a9a5f4dcb047d915e3006d9947181d0
Justin <info@justinle.me>
2022-04-06 17:32:28 -0700

    solved, slow though

--- solved/LeetCode/1021_removeOutermostParens.js
+++ solved/LeetCode/1021_removeOutermostParens.js
@@ -15,4 +15,41 @@ S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
 
 Return S after removing the outermost parentheses of every primitive string
 in the primitive decomposition of S.
+
+Example 1:
+Input: s = "(()())(())"
+Output: "()()()"
+
+Example 2:
+s = "(()())(())(()(()))";
+Output: "()()()()(())"
+
+Example 3:
+s = "()()";
+Output: ""
 */
+
+const removeOuterParentheses = (s) => {
+    let parens = 0;
+    let result = ``;
+
+    for (const paren of s) {
+        if (paren === `(`) {
+            if (parens > 0) {
+                result += paren;
+            }
+            parens++;
+        } else {
+            parens--;
+            if (parens > 0) {
+                result += paren;
+            }
+        }
+    }
+    return result;
+};
+
+removeOuterParentheses(`(()())(())(()(()))`); // "()()()()(())"
+
+// 136ms, faster than 11% of js submissions
+// 43.7mb, less than 81.10% of js submissions
