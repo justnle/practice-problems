e0d26c97c214e583670a2a252d95fcf0b1ed4c8c
Justin <info@justinle.me>
2022-04-11 20:59:27 -0700

    resolved, blind 75

--- solved/LeetCode/0125_validPalindrome.js
+++ solved/LeetCode/0125_validPalindrome.js
@@ -24,3 +24,15 @@ const isPalindrome = (s) => {
 };
 
 isPalindrome(`race a car`); // false
+
+const isPalindrome2 = (s) => {
+    const str = s.replace(/[a-z0-9]/gi, ``).toLowerCase();
+    const rev = str.split(``).reverse().join(``);
+
+    return str === rev;
+};
+
+isPalindrome2(`A man, a plan, a canal: Panama`); // true;
+
+// 86ms, faster than 71.95% of js submissons
+// 46.3mb, less than 47.21% of js submissions
