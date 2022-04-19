d69a63ab428763615ce2d3c62cea2099b60ae25a
Justin <info@justinle.me>
2022-04-19 00:20:35 -0700

    improved algo, faster

--- solved/LeetCode/0036_validSudoku.js
+++ solved/LeetCode/0036_validSudoku.js
@@ -43,76 +43,43 @@ Output: false
 */
 
 const isValidSudoku = (board) => {
-    for (let row = 0; row < board.length; ++row) {
-        const rowMap = new Map();
-        const colMap = new Map();
+    for (let i = 0; i < 9; ++i) {
+        const row = new Set();
+        const col = new Set();
+        const grid = new Set();
 
-        for (let col = 0; col < board.length; ++col) {
-            const rowElem = board[row][col];
-            const colElem = board[col][row];
-
-            if (rowMap.has(rowElem) || colMap.has(colElem)) {
-                return false;
-            } else {
-                if (rowElem !== `.`) {
-                    rowMap.set(rowElem, row);
-                }
-
-                if (colElem !== `.`) {
-                    colMap.set(colElem, row);
-                }
-            }
-        }
-    }
-
-    const gridMap = new Map();
-    let gridRow = 3;
-    let gridCol = 3;
-    let addCol = 0;
-    let grid = 1;
-
-    for (let row = 0; row < gridRow; ++row) {
-        for (let col = 0 + addCol; col < gridCol; ++col) {
-            const gridElem = board[row][col];
-            const mapVals = [...gridMap.values()];
-
-            if (gridElem !== `.` && mapVals.includes(gridElem)) {
-                return false;
-            }
-
-            if (gridMap.has(gridElem) && gridElem !== `.`) {
-                return false;
-            } else {
-                gridMap.set(`row: ${row} col: ${col} grid: ${grid}`, gridElem);
-            }
-
-            if (gridMap.size === 9) {
-                console.log(gridMap);
-
-                gridMap.clear();
-                ++grid;
-                // console.log(`reset row`);
-                row -= 2;
-
-                if (gridCol < 9) {
-                    gridCol += 3;
-                }
-
-                if (addCol < 9) {
-                    addCol += 3;
-                }
-            }
-        }
-
-        if (
+        for (let j = 0; j < 9; ++j) {
+            const rowElem = board[i][j];
+            const colElem = board[j][i];
             // eslint-disable-next-line operator-linebreak
-            (grid === 4 && gridMap.size === 0) ||
-            (grid === 7 && gridMap.size === 0)
-        ) {
-            gridCol = 3;
-            addCol = 0;
-            gridRow += 3;
-            row += 2;
+            const gridElem =
+                board[Math.floor(i / 3) * 3 + Math.floor(j / 3)][
+                    (i % 3) * 3 + (j % 3)
+                ];
+
+            if (rowElem !== `.`) {
+                if (row.has(rowElem)) {
+                    return false;
+                } else {
+                    row.add(rowElem);
+                }
+            }
+
+            if (colElem !== `.`) {
+                if (col.has(colElem)) {
+                    return false;
+                } else {
+                    col.add(colElem);
+                }
+            }
+
+            if (gridElem !== `.`) {
+                if (grid.has(gridElem)) {
+                    return false;
+                } else {
+                    grid.add(gridElem);
+                }
+            }
         }
     }
     return true;
@@ -142,5 +109,5 @@ isValidSudoku([
     [`.`, `.`, `.`, `.`, `8`, `.`, `.`, `7`, `9`]
 ]); // false
 
-// 237ms, faster than 5.01% of js submissions
-// 50.5mb, less than 8.14% of js submissions
+// 87ms, faster than 77.65% of js submissions
+// 44.5mb, less than 91.68% of js submissions
