0dcd2f491a2a5038a8374ee1d546847a2bfd66de
Justin <info@justinle.me>
2022-06-08 00:23:59 -0700

    cleaned up

--- interview/kingDomino.js
+++ interview/kingDomino.js
@@ -73,9 +73,9 @@ const crowns = [
 ];
 
 const kingDomino = (board, crowns) => {
+    const terrains = {};
     let groupCount = 0;
     let points = 0;
-    const terrains = {};
 
     for (let i = 0; i < board.length; ++i) {
         for (let j = 0; j < board[i].length; ++j) {
